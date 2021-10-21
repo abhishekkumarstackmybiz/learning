9a5e75da2c855737c867b5557731108ba98b3254
abhishekkumarstackmybiz <abhishek.kumar@stackmybiz.com>
2021-10-21 10:34:45 +0530

    hiding .gitignore file

--- src/DiviserSumClass.java
+++ src/DiviserSumClass.java
@@ -3,9 +3,9 @@ import java.util.stream.IntStream;
 
 public class DiviserSumClass {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        Integer i=IntStream.range(1,n+1).filter(e->n%e==0).reduce(0,(n1,n2)->n1+n2);
-        System.out.println("Sum:-"+i);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer i = IntStream.range(1, n + 1).filter(e -> n % e == 0).reduce(0, (n1, n2) -> n1 + n2);
+        System.out.println("Sum:-" + i);
     }
 }
